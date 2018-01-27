f2def0266090a3dd478dd42b4e697e92091b65fa
muttsu-623 <mutsumi1996@gmail.com>
2018-01-27 10:59:11 +0900

    Initial Commit

---
+++ MapData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapData
+//
+//  Created by 佐藤夢積 on 2018/01/27.
+//  Copyright © 2018年 Mutsumi Sato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
